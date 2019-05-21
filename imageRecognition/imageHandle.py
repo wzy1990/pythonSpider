ceb9a6df77714a732123bbd4af9efc1e70f7aa2c
946455381@qq.com <wzy19900420>
2019-05-21 23:07:51 +0800

    修改图像识别，支持循环识别

--- imageRecognition/imageHandle.py
+++ imageRecognition/imageHandle.py
@@ -2,15 +2,18 @@ import keyboard # pip install keyboard
 from PIL import ImageGrab #pip install pillow
 from imageRecognition.aip_image import get_info_from_image
 
-keyboard.wait(hotkey='ctrl+alt+a') # 等待截图，Ctrl + alt + a 是QQ的截图快捷键
-print('start')
-keyboard.wait(hotkey='ctrl+f1')
-print('end')
+count = 1
 
+while count <= 10:
+    keyboard.wait(hotkey='ctrl+alt+a')  # 等待截图，Ctrl + alt + a 是QQ的截图快捷键
+    print('start')
+    keyboard.wait(hotkey='ctrl+f1')
+    print('开始分析图像：')
 
-image = ImageGrab.grabclipboard()
-if image:
-    image.save('screen.png')
-    print(get_info_from_image('screen.png'))
-else:
-    print('请截图哦')
+    image = ImageGrab.grabclipboard()
+    if image:
+        count += 1
+        image.save('screen.png')
+        print(get_info_from_image('screen.png'))
+    else:
+        print('请截图哦')
